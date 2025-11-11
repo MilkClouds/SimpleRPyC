3931ff95f387d96ee4f97c31c486135b55444140
MilkClouds <milkclouds00@gmail.com>
2025-11-11 22:28:24 +0900

    Remove unused import of 'patch' from __init__.py

--- simplerpc/__init__.py
+++ simplerpc/__init__.py
@@ -1,7 +1,7 @@
 """SimpleRPC - Simple Remote Procedure Call over WebSocket."""
 
 from simplerpc.client.connection import connect, disconnect
-from simplerpc.client.patcher import patch, patch_module
+from simplerpc.client.patcher import patch_module
 from simplerpc.client.proxy import is_proxy, materialize
 
 __version__ = "0.1.0"
@@ -9,7 +9,6 @@ __version__ = "0.1.0"
 __all__ = [
     "connect",
     "disconnect",
-    "patch",
     "patch_module",
     "materialize",
     "is_proxy",
